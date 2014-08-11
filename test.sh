e1e60fb8fed74dd44b309346307355ce05fbb620
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-08-11 18:31:44 -0400

    Restore install

--- test.sh
+++ test.sh
@@ -13,8 +13,8 @@ HTTPSE_INSTALL_DIRECTORY=$PROFILE_DIRECTORY/extensions/https-everywhere@eff.org
 ./makexpi.sh
 XPI_NAME="pkg/`ls -tr pkg/ | tail -1`"
 
-#rsync -a https-everywhere-tests/test_profile_skeleton/ $PROFILE_DIRECTORY
-#unzip -qd $HTTPSE_INSTALL_DIRECTORY $XPI_NAME
+rsync -a https-everywhere-tests/test_profile_skeleton/ $PROFILE_DIRECTORY
+unzip -qd $HTTPSE_INSTALL_DIRECTORY $XPI_NAME
 
 if [ ! -d "$TEST_ADDON_PATH" ]; then
   echo "Test addon path does not exist"
