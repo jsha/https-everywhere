70a96518d467ed3e6a853c144fae7fbbfb4c6486
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2014-12-09 17:33:14 -0800

    Enable debugging in test skeleton profile.

--- https-everywhere-tests/test_profile_skeleton/prefs.js
+++ https-everywhere-tests/test_profile_skeleton/prefs.js
@@ -4,7 +4,7 @@ user_pref("extensions.https_everywhere._observatory.enabled", true);
 user_pref("extensions.https_everywhere._observatory.popup_shown", true);
 user_pref("extensions.https_everywhere.toolbar_hint_shown", true);
 // Show all logs.
-user_pref("extensions.https_everywhere.LogLevel", 0);
+user_pref("extensions.https_everywhere.LogLevel", 3);
 user_pref("extensions.https_everywhere.log_to_stdout", true);
 // Allow running of Mixed Content Blocking tests.
 user_pref("extensions.https_everywhere.show_ruleset_tests", true);
@@ -13,3 +13,6 @@ user_pref("general.warnOnAboutConfig", false);
 // Minimize unnecesary requests.
 user_pref("browser.safebrowsing.enabled", false);
 user_pref("browser.safebrowsing.malware.enabled", false);
+// Enable debugging by default.
+user_pref("devtools.chrome.enabled", true);
+user_pref("devtools.debugger.remote-enabled", true);
