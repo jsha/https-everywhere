8416bfaab80fa277ac664a5d63ce45868b6aad44
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2015-01-30 22:30:04 -0800

    Add a basic ruleset tester.

---
+++ rewriter/tester.js
@@ -0,0 +1,125 @@
+// HTTPS Ruleset tester
+// Generate a set of test URLs by reading in each of the rulesets and using the
+// host from each target. Targets with a left-side wildcard ("*.foo.com") are
+// treated as www. Targets with a wildcard elsewhere are ignored for now.
+//
+// Then, for each of the test URLs:
+//  - Run it through the rulesets.
+//  - Check that it changed, if not fail that URL.
+//  - Fetch the rewritten URL, following redirects.
+//  - If there any of the following occur, fail the original URL:
+//    - connection failure
+//    - connection timeout (30 seconds)
+//    - non-200 final status code
+//    - more than 10 redirects
+
+var path = require("path"),
+    fs = require("fs"),
+    DOMParser = require('xmldom').DOMParser,
+    readdirp = require('readdirp'),
+    es = require('event-stream'),
+    async = require('async'),
+    lrucache = require("../chromium/lru"),
+    rules = require("../chromium/rules"),
+    request = require("request"),
+    URI = require("URIjs");
+
+var ruleSets = null;
+
+var cases = [];
+var ruleSets = new rules.RuleSets("fake user agent", lrucache.LRUCache, {});
+
+function runTests() {
+  var q = async.queue(runTestCase, 50);
+  q.drain = function() {
+    console.log("All done.");
+  }
+  q.push(cases);
+}
+
+function runTestCase(testCase, callback) {
+  var uriString = testCase.url;
+  var ruleFile = testCase.ruleFile;
+  var line = ruleFile + " " + uriString;
+  var uri = new URI(uriString);
+  var rewritten = ruleSets.rewriteURI(uri.toString(), uri.host());
+  if (!rewritten) {
+    console.log("FAIL: no rewrite: ", line);
+    return callback();
+  } else {
+    line = ruleFile + " " + uriString + " " + rewritten;
+    console.log("Requesting:", rewritten);
+    request({
+      uri: rewritten,
+      maxRedirects: 10,
+      timeout: 10000, /* ms */
+      gzip: true,
+      strictSSL: true
+    }, function(error, response, body) {
+      if (error) {
+        console.log("FAIL:", error.message.replace(/\n/g, ' '), ":", line);
+        return callback();
+      } else if (response.statusCode !== 200) {
+        console.log("FAIL: status code", response.statusCode, ":", line);
+        return callback();
+      } else {
+        console.log("PASS: ", uriString);
+        return callback();
+      }
+    });
+  }
+};
+
+function readXml(dir) {
+  var stream = readdirp({
+    root: dir,
+    fileFilter: ['*.xml']
+  });
+
+  stream
+  .on('error', function (err) { console.error('fatal error', err); })
+  .pipe(es.map(function (entry, callback) {
+    var filename = path.join(dir, entry.path);
+    fs.readFile(filename, function(err, fileContents) {
+      var xml = new DOMParser().parseFromString(fileContents.toString('utf8'), 'text/xml');
+      ruleSets.addFromXml(xml);
+      var rulesets = xml.getElementsByTagName('ruleset');
+      if (rulesets.length > 1) {
+        console.log('FAIL: more than one ruleset in', filename);
+        process.exit(1);
+      }
+      addTestCases(rulesets[0], filename);
+      callback();
+    });
+  })).on('end', function(err, body) {
+    runTests();
+  });
+}
+
+function addTestCases(xml, filename) {
+  if (xml.getAttribute('default_off') || xml.getAttribute('platform')) {
+    //console.log('Skipping inactive ruleset', filename);
+    return;
+  }
+  var targets = xml.getElementsByTagName('target');
+  for (var i = 0; i < targets.length; i++) {
+    var targetTag = targets[i];
+    var target = targetTag.getAttribute('host');
+    if (target[0] === '*') {
+      cases.push({
+        url: 'http://www.' + target.slice(2),
+        ruleFile: filename
+      });
+    } else if (target.match(/\*/)) {
+      // skip
+    } else {
+      cases.push({
+        url: 'http://' + target,
+        ruleFile: filename
+      });
+    }
+  };
+}
+
+console.log("Loading rules...");
+readXml('../src/chrome/content/rules');
